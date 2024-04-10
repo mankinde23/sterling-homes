b660a5e89f0feaa2b8aea1cec8813ffebcf508fa
mankinde23 <damilolamakinde493@gmail.com>
2024-04-10 09:04:36 +0100

    added a new component folder

---
+++ src/app/component/darmodetoggle/darkmodetoggle.jsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { TbBrightnessUp } from "react-icons/tb";
+import { FaMoon } from "react-icons/fa";
+
+function DarkModeToggle() {
+  //   const { themeMode, toggleTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  //   if (!mounted) {
+  //     return null;
+  //   }
+  //   const toggleTheme = () => {
+  //     setTheme(theme === "light" ? "dark" : "light");
+  //   };
+  useEffect(() => {
+    if (mounted) {
+      const savedTheme = localStorage.getItem("theme");
+      if (savedTheme) {
+        setTheme(savedTheme);
+      }
+    }
+  }, [mounted, setTheme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "light" ? (
+        <FaMoon className=" h-[1.73rem] w-[2.5rem] text-nav-text" />
+      ) : (
+        <TbBrightnessUp className="  h-[1.73rem] w-[2.5rem] text-nav-text " />
+      )}
+    </button>
+  );
+}
+
+export default DarkModeToggle;
---
+++ src/app/component/navbar/navbar.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import logo from "../../../../public/sterling-logo.png";
+import Link from "next/link";
+import DarkModeToggle from "../darmodetoggle/darkmodetoggle";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-full z-50 h-[4.4375rem]  flex justify-between items-center backdrop-blur-[7px] bg-navbar-bg">
+        <div className="ml-[5.94rem]">
+          <Link href="/">
+            <Image
+              src={logo}
+              alt=""
+              quality={100}
+              className="w-[91px] h-[64.55px]"
+            />
+          </Link>
+        </div>
+        <div>
+          <ul className="flex justify-between items-center w-[32.5rem] gap-[1.09rem] px-[1.25rem] py-[0rem] h-[2.375rem]">
+            <li className=" text-nav-text">
+              <Link href="">Home </Link>
+            </li>
+            <li className=" text-nav-text">
+              <Link href="">About </Link>
+            </li>
+            <li className=" text-nav-text">
+              <Link href="">Services </Link>
+            </li>
+            <li className=" text-nav-text">
+              <Link href="">Testimonials </Link>
+            </li>
+            <li className=" text-nav-text">
+              <Link href="">Contact </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="mr-[4.69rem]">
+          <DarkModeToggle />
+        </div>
+      </div>
+    </>
+  );
+}
