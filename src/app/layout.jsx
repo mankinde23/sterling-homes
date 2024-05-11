7ee9cda0fbc1b60f1abaa48e9ffb5be0f5f4d2a3
mankinde23 <damilolamakinde493@gmail.com>
2024-05-11 17:29:25 +0100

    removed the navbar tag from the layout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers>
           <div className="theme">
-            <Navbar />
+            {/* <Navbar /> */}
             {children}
           </div>
         </Providers>
