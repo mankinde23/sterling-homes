fe858041a7dbd00a8bf1b19aeaba08b26fe8c741
mankinde23 <damilolamakinde493@gmail.com>
2024-05-14 17:14:55 +0100

    started the mobile view section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,52 +11,59 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <div
-        className="bg-cover h-[57.3125rem] w-auto bg-light-gray bg-no-repeat flex-shrink-0  scale-100 "
-        style={{
-          backgroundImage: "url('/landing-img2.png')",
-        }}
-        id="hero"
-      >
-        <div className="flex justify-center items-end ">
-          <div className=" mt-[19.88rem]">
-            <h1 className="text-nav-text font-ameretto text-[4.6875rem] leading-normal tracking-[0.9375rem] font-normal">
-              STERLING HOMES
-            </h1>
-            <p className="flex justify-center items-center text-nav-text text-[0.8125rem] font-light">
-              Building dreams, one home at a time, with integrity and excellence
-            </p>
-            <div className="flex justify-center items-center mt-[4rem]">
-              <Link href="/catalogue">
-                <button className="w-[11.9375rem] h-[2.875rem] p-[0.5rem] gap-[0.5rem] bg-sterling-theme">
-                  <p className="text-nav-text text-[1rem] font-semibold leading-normal">
-                    View Catalogue
-                  </p>
-                </button>
-              </Link>
+      <div className="sm:hidden 2xl:block 2xl-max:block xl:block lg:block  md:block">
+        <Navbar />
+        <div
+          className="bg-cover h-[57.3125rem] w-auto bg-light-gray bg-no-repeat flex-shrink-0  scale-100 "
+          style={{
+            backgroundImage: "url('/landing-img2.png')",
+          }}
+          id="hero"
+        >
+          <div className="flex justify-center items-end ">
+            <div className=" mt-[19.88rem]">
+              <h1 className="text-nav-text font-ameretto text-[4.6875rem] leading-normal tracking-[0.9375rem] font-normal">
+                STERLING HOMES
+              </h1>
+              <p className="flex justify-center items-center text-nav-text text-[0.8125rem] font-light">
+                Building dreams, one home at a time, with integrity and
+                excellence
+              </p>
+              <div className="flex justify-center items-center mt-[4rem]">
+                <Link href="/catalogue">
+                  <button className="w-[11.9375rem] h-[2.875rem] p-[0.5rem] gap-[0.5rem] bg-sterling-theme">
+                    <p className="text-nav-text text-[1rem] font-semibold leading-normal">
+                      View Catalogue
+                    </p>
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
+        <div>
+          <Aboutus />
+        </div>
+        <div>
+          <Ourservice />
+        </div>
+        <div>
+          <Testimonial />
+        </div>
+        <div>
+          <Founders />
+        </div>
+        <div>
+          <Contact />
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
-      <div>
-        <Aboutus />
-      </div>
-      <div>
-        <Ourservice />
-      </div>
-      <div>
-        <Testimonial />
-      </div>
-      <div>
-        <Founders />
-      </div>
-      <div>
-        <Contact />
-      </div>
-      <div>
-        <Footer />
-      </div>
+
+      {/* MOBILE */}
+
+      <div className="2xl:hidden 2xl-max:hidden xl:hidden lg:hidden sm:block md:hidden"></div>
     </>
   );
 }
