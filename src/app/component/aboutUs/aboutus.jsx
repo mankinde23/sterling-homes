ee587b959c828968b6f3f6b9cc760aff66357df2
mankinde23 <damilolamakinde493@gmail.com>
2024-05-19 07:54:37 +0100

    styled the mobile view to be responsive as possible

--- src/app/component/aboutUs/aboutus.jsx
+++ src/app/component/aboutUs/aboutus.jsx
@@ -124,117 +124,112 @@ export default function Aboutus() {
 
       <div className="2xl:hidden 2xl-max:hidden xl:hidden lg:hidden sm:block md:hidden">
         <div
-          className={`h-[68.125rem] w-auto  px-[1.87rem]  ${
+          className={`h-[68.125rem] w-auto  px-[1.87rem] pt-[3.06rem] ${
             theme === "dark"
               ? "bg-about-bg transition duration-500 ease-in-out"
               : "bg-nav-text transition duration-500 ease-in-out"
           }`}
           id="about"
         >
-          <div className="flex  justify-between gap-[1.75rem] items-center pt-[3.06rem] flex-col">
-            <div>
-              <div className="h-[2.3125rem]">
-                <h1
-                  className={` text-[1.5rem] font-bold leading-normal ${
-                    theme === "light" ? "text-about-b" : "text-nav-text"
-                  }`}
-                >
-                  ABOUT US
-                </h1>
-                <p className=" text-sterling-theme text-[0.4375rem] font-medium leading-[0.105rem] mt-[0.25rem]">
-                  Est . 2024, March.
-                </p>
-              </div>
-              <div>
-                <Image
-                  src={mock}
-                  alt=""
-                  className="w-[20.625rem] mt-[1.38rem]"
-                />
-              </div>
-            </div>
-            <div className=" h-[18.5rem] w-auto text-justify hyphens-auto">
-              <p
-                className={` text-[0.6875rem] font-normal  ${
-                  theme === "light" ? "text-about-t" : "text-nav-text"
-                }`}
-              >
-                Established on June 11, 2022, by accomplished founders with
-                extensive experience in technology across critical sectors in
-                the United States, our mission is to ensure customers enjoy an
-                exceptional experience throughout the entire lifecycle of home
-                ownership. From the initial search for a home to the purchase
-                process, ongoing house maintenance, and optimizing tax benefits
-                for both residential and commercial properties, we are committed
-                to delivering excellence. Through proactive measures to identify
-                and assess property risks, we have successfully closed over 50
-                homes for our customers since our inception, with a cumulative
-                market value exceeding $20,000,000. We take pride in achieving
-                appreciable equity growth for all our clients, distinguishing
-                ourselves in both residential and commercial real estate
-                sectors.
-              </p>
-            </div>
+          <div className="h-[2.5625rem] w-auto">
+            <h1
+              className={` text-[1.5rem] font-bold leading-normal  ${
+                theme === "light" ? "text-about-b" : "text-nav-text"
+              }`}
+              style={{ fontFamily: " Montserrat, sans-serif" }}
+            >
+              ABOUT US
+            </h1>
+            <p
+              className=" text-sterling-theme text-[0.4375rem]
+               font-medium leading-normal] mt-[0.25rem] tracking-[0.0525rem]"
+              style={{ fontFamily: " Montserrat, sans-serif" }}
+            >
+              Est . 2024, March.
+            </p>
           </div>
-          <div className="flex flex-col justify-between items-center mt-[3.94rem] w-auto h-[24.43rem] ">
-            <div className="flex flex-col gap-[0.81rem] w-auto ">
-              <p
-                className={` text-[0.875rem] font-semibold ${
-                  theme === "light" ? "text-about-t" : "text-nav-text"
-                }`}
-              >
-                Commercial and Luxury Homes Focus
-              </p>
-              <p
-                className={` text-[0.6875rem] font-normal leading-normal h-[6rem]  text-justify  hyphens-auto ${
-                  theme === "light" ? "text-about-s-w" : "text-about-s-b"
-                }`}
-              >
-                Our company specializes in commercial and luxury home real
-                estate, offering tailored solutions to meet client needs,
-                ensuring seamless transactions, and delivering exceptional value
-                and service.
-              </p>
-            </div>
-            <div className="flex flex-col gap-[0.81rem] w-auto  mt-[0.31rem]">
-              <p
-                className={` text-[0.875rem] font-semibold ${
-                  theme === "light" ? "text-about-t" : "text-nav-text"
-                }`}
-              >
-                Realtor Services Description
-              </p>
-              <p
-                className={`text-[0.6875rem] font-normal leading-normal h-[6rem]  text-justify hyphens-auto ${
-                  theme === "light" ? "text-about-s-w" : "text-about-s-b"
-                }`}
-              >
-                Our realtor services provide expert guidance in buying or
-                selling properties, ensuring a smooth process, maximizing value,
-                and delivering personalized solutions to meet your real estate
-                goals.
-              </p>
-            </div>
+          <div className="mt-[1.38rem]">
+            <Image src={mock} alt="" className="w-auto " />
+          </div>
 
-            <div className="flex flex-col gap-[0.81rem] w-auto  mt-[0.31rem]">
-              <p
-                className={` text-[0.875rem] font-semibold ${
-                  theme === "light" ? "text-about-t" : "text-nav-text"
-                }`}
-              >
-                Tax Advisory
-              </p>
-              <p
-                className={`text-[0.6875rem] font-normal leading-normal h-[6rem] text-justify hyphens-auto ${
-                  theme === "light" ? "text-about-s-w" : "text-about-s-b"
-                }`}
-              >
-                Our tax advisory services offer expert guidance on tax planning
-                and compliance, optimizing financial strategies, ensuring
-                regulatory compliance, and maximizing tax savings for
-                individuals and businesses.
-              </p>
-            </div>
+          <div className=" w-auto text-justify hyphens-auto mt-[1.75rem]">
+            <p
+              className={` text-[0.6875rem] font-normal  ${
+                theme === "light" ? "text-about-t" : "text-nav-text"
+              }`}
+            >
+              Established on June 11, 2022, by accomplished founders with
+              extensive experience in technology across critical sectors in the
+              United States, our mission is to ensure customers enjoy an
+              exceptional experience throughout the entire lifecycle of home
+              ownership. From the initial search for a home to the purchase
+              process, ongoing house maintenance, and optimizing tax benefits
+              for both residential and commercial properties, we are committed
+              to delivering excellence. Through proactive measures to identify
+              and assess property risks, we have successfully closed over 50
+              homes for our customers since our inception, with a cumulative
+              market value exceeding $20,000,000. We take pride in achieving
+              appreciable equity growth for all our clients, distinguishing
+              ourselves in both residential and commercial real estate sectors.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-[0.81rem] w-auto mt-[3.94rem]">
+            <p
+              className={` text-[0.875rem] font-semibold ${
+                theme === "light" ? "text-about-t" : "text-nav-text"
+              }`}
+            >
+              Commercial and Luxury Homes Focus
+            </p>
+            <p
+              className={` text-[0.6875rem] font-normal leading-normal h-[6rem]  text-justify  hyphens-auto ${
+                theme === "light" ? "text-about-s-w" : "text-about-s-b"
+              }`}
+            >
+              Our company specializes in commercial and luxury home real estate,
+              offering tailored solutions to meet client needs, ensuring
+              seamless transactions, and delivering exceptional value and
+              service.
+            </p>
+          </div>
+          <div className="flex flex-col gap-[0.81rem] w-auto  mt-[0.31rem]">
+            <p
+              className={` text-[0.875rem] font-semibold ${
+                theme === "light" ? "text-about-t" : "text-nav-text"
+              }`}
+            >
+              Realtor Services Description
+            </p>
+            <p
+              className={`text-[0.6875rem] font-normal leading-normal h-[6rem]  text-justify hyphens-auto ${
+                theme === "light" ? "text-about-s-w" : "text-about-s-b"
+              }`}
+            >
+              Our realtor services provide expert guidance in buying or selling
+              properties, ensuring a smooth process, maximizing value, and
+              delivering personalized solutions to meet your real estate goals.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-[0.81rem] w-auto  mt-[0.31rem]">
+            <p
+              className={` text-[0.875rem] font-semibold ${
+                theme === "light" ? "text-about-t" : "text-nav-text"
+              }`}
+            >
+              Tax Advisory
+            </p>
+            <p
+              className={`text-[0.6875rem] font-normal leading-normal h-[6rem] text-justify hyphens-auto ${
+                theme === "light" ? "text-about-s-w" : "text-about-s-b"
+              }`}
+            >
+              Our tax advisory services offer expert guidance on tax planning
+              and compliance, optimizing financial strategies, ensuring
+              regulatory compliance, and maximizing tax savings for individuals
+              and businesses.
+            </p>
           </div>
         </div>
       </div>
