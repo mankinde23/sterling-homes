7d9c91beb241dcdc603f5df9e71ee1ab58ff90c4
mankinde23 <damilolamakinde493@gmail.com>
2024-05-19 07:50:25 +0100

    styled the mobile view to be responsive as possible

--- src/app/component/testimonial/testimonial.jsx
+++ src/app/component/testimonial/testimonial.jsx
@@ -9,19 +9,58 @@ export default function Testimonial() {
   const { theme } = useTheme();
   return (
     <>
-      <div
-        className={`h-[47.875rem] w-auto py-[4.44rem] px-[3.88rem]  ${
-          theme === "dark"
-            ? "bg-about-bg transition duration-500 ease-in-out"
-            : "bg-nav-text transition duration-500 ease-in-out"
-        }`}
-        id="testimonials"
-      >
-        <div className="flex  gap-[3.56rem]">
-          <Image src={test} alt="" className="w-[55.3125rem] h-[39rem]" />
-          <div className="w-[13rem] h-[4.5625rem] flex flex-col items-center gap-[0.25rem]">
+      <div className="sm:hidden 2xl:block 2xl-max:block xl:block lg:block  md:block">
+        <div
+          className={`h-[47.875rem] w-auto py-[4.44rem] px-[3.88rem]  ${
+            theme === "dark"
+              ? "bg-about-bg transition duration-500 ease-in-out"
+              : "bg-nav-text transition duration-500 ease-in-out"
+          }`}
+          id="testimonials"
+        >
+          <div className="flex  gap-[3.56rem]">
+            <Image src={test} alt="" className="w-[55.3125rem] h-[39rem]" />
+            <div className="w-[13rem] h-[4.5625rem] flex flex-col items-center gap-[0.25rem]">
+              <p
+                className={` text-[2rem] font-semibold leading-normal ${
+                  theme === "light"
+                    ? " text-sterling-theme"
+                    : "text-our-service-text"
+                }`}
+              >
+                Testimonials
+              </p>
+              <p
+                className={` text-[0.75rem] font-semibold leading-normal mb-[2.19rem] ${
+                  theme === "light"
+                    ? " text-testimonial-text"
+                    : "text-sterling-theme"
+                }`}
+              >
+                what Our Clients Say
+              </p>
+              <div className="mb-[2.19rem]">
+                <Carousel />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile */}
+
+      <div className="2xl:hidden 2xl-max:hidden xl:hidden lg:hidden sm:block md:hidden ">
+        <div
+          className={`h-[24.125rem] w-auto pt-[2.38rem] pr-[1.88rem] pb-[1.2rem] pl-[1.88rem] flex flex-col gap-[0.69rem] items-center ${
+            theme === "dark"
+              ? "bg-about-bg transition duration-500 ease-in-out"
+              : "bg-nav-text transition duration-500 ease-in-out"
+          }`}
+          id="testimonials"
+        >
+          <div className="w-[8.5625rem] h-[3rem] flex flex-col gap-[0.1rem] items-center">
             <p
-              className={` text-[2rem] font-semibold leading-normal ${
+              className={` text-[1rem] font-semibold leading-normal ${
                 theme === "light"
                   ? " text-sterling-theme"
                   : "text-our-service-text"
@@ -30,7 +69,7 @@ export default function Testimonial() {
               Testimonials
             </p>
             <p
-              className={` text-[0.75rem] font-semibold leading-normal mb-[2.19rem] ${
+              className={` text-[0.6875rem] font-medium leading-normal ${
                 theme === "light"
                   ? " text-testimonial-text"
                   : "text-sterling-theme"
@@ -38,9 +77,10 @@ export default function Testimonial() {
             >
               what Our Clients Say
             </p>
-            <div className="mb-[2.19rem]">
-              <Carousel />
-            </div>
+          </div>
+
+          <div>
+            <Carousel />
           </div>
         </div>
       </div>
