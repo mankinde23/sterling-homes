1847ae0885af6244292c16577b6cdd7b8e9ed28c
mankinde23 <damilolamakinde493@gmail.com>
2024-05-26 14:42:25 +0100

    added a new component for the hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,40 +7,18 @@ import Contact from "./component/contact/contact";
 import Footer from "./component/footer/footer";
 import Navbar from "./component/navbar/navbar";
 import Link from "next/link";
+import Hero from "./component/hero/hero";
 
 export default function Home() {
   return (
     <>
       <div className="sm:hidden 2xl:block 2xl-max:block xl:hidden lg:hidden  md:hidden">
         <Navbar />
-        <div
-          className="bg-cover h-[57.3125rem] w-auto bg-light-gray bg-no-repeat flex-shrink-0  scale-100 "
-          style={{
-            backgroundImage: "url('/landing-img2.png')",
-          }}
-          id="hero"
-        >
-          <div className="flex justify-center items-end ">
-            <div className=" mt-[19.88rem]">
-              <h1 className="text-nav-text font-ameretto text-[4.6875rem] leading-normal tracking-[0.9375rem] font-normal">
-                STERLING HOMES
-              </h1>
-              <p className="flex justify-center items-center text-nav-text text-[0.8125rem] font-light">
-                Building dreams, one home at a time, with integrity and
-                excellence
-              </p>
-              <div className="flex justify-center items-center mt-[4rem]">
-                <Link href="/catalogue">
-                  <button className="w-[11.9375rem] h-[2.875rem] p-[0.5rem] gap-[0.5rem] bg-sterling-theme hover:bg-sterling-hover transition duration-500 ease-in-out">
-                    <p className="text-nav-text text-[1rem] font-light leading-normal">
-                      View Catalogue
-                    </p>
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
+
+        <div className="w-full " id="hero">
+          <Hero />
         </div>
+
         <div className="w-full " id="about">
           <Aboutus />
         </div>
@@ -65,34 +43,10 @@ export default function Home() {
 
       <div className="sm:hidden 2xl:hidden 2xl-max:hidden xl:block lg:block  md:block">
         <Navbar />
-        <div
-          className="bg-cover h-[57.3125rem] w-auto bg-light-gray bg-no-repeat flex-shrink-0  scale-100 "
-          style={{
-            backgroundImage: "url('/landing-img2.png')",
-          }}
-          id="heromm"
-        >
-          <div className="flex justify-center items-end ">
-            <div className=" mt-[19.88rem]">
-              <h1 className="text-nav-text font-ameretto text-[4.6875rem] leading-normal tracking-[0.9375rem] font-normal">
-                STERLING HOMES
-              </h1>
-              <p className="flex justify-center items-center text-nav-text text-[0.8125rem] font-light">
-                Building dreams, one home at a time, with integrity and
-                excellence
-              </p>
-              <div className="flex justify-center items-center mt-[4rem]">
-                <Link href="/catalogue">
-                  <button className="w-[11.9375rem] h-[2.875rem] p-[0.5rem] gap-[0.5rem] bg-sterling-theme hover:bg-sterling-hover transition duration-500 ease-in-out">
-                    <p className="text-nav-text text-[1rem] font-light leading-normal">
-                      View Catalogue
-                    </p>
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
+        <div className="w-full " id="heromm">
+          <Hero />
         </div>
+
         <div className="w-full scroll-mt-[3.5rem] " id="aboutmm">
           <Aboutus />
         </div>
@@ -118,34 +72,10 @@ export default function Home() {
       <div className="2xl:hidden 2xl-max:hidden xl:hidden lg:hidden sm:block md:hidden ">
         <Navbar />
 
-        <div
-          className="bg-cover h-[42.0625rem] w-full bg-light-gray bg-no-repeat flex-shrink-0  scale-100 "
-          style={{
-            backgroundImage: "url('/sterling-bg-m.png')",
-          }}
-          id="herom"
-        >
-          <div className="flex justify-center items-end w-full">
-            <div className=" mt-[17.69rem]">
-              <h1 className="text-nav-text font-ameretto text-[1.875rem] leading-normal  font-normal tracking-[0.375rem]">
-                STERLING HOMES
-              </h1>
-              <p className="flex justify-center items-center text-nav-text text-[0.4375rem]  font-extralight">
-                Building dreams, one home at a time, with integrity and
-                excellence
-              </p>
-              <div className="flex justify-center items-center mt-[1.56rem]">
-                <Link href="/catalogue">
-                  <button className="w-[6.6875rem] h-[1.6875rem] p-[0.5rem] gap-[0.5rem] bg-sterling-theme">
-                    <p className="text-nav-text text-[0.625rem] font-light leading-normal">
-                      View Catalogue
-                    </p>
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
+        <div className="w-full " id="herom">
+          <Hero />
         </div>
+
         <div className="w-full scroll-mt-[3.5rem]" id="aboutm">
           <Aboutus />
         </div>
