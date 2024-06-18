4e790f1c14d996e70dae2deeffc23c7bdd9ac8f4
mankinde23 <damilolamakinde493@gmail.com>
2024-06-18 20:16:05 +0100

    testing style error 2

--- src/app/component/aboutUs/aboutus.jsx
+++ src/app/component/aboutUs/aboutus.jsx
@@ -272,7 +272,7 @@ export default function Aboutus() {
 
           <div className="flex flex-col gap-[0.81rem] w-auto mt-[10rem]">
             <p
-              className={` text-[0.875rem] font-semibold ${
+              className={` text-[0.875rem] font-medium ${
                 theme === "light" ? "text-about-t" : "text-nav-text"
               }`}
               style={{ fontFamily: " Montserrat, sans-serif" }}
