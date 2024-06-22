1891da0223db9baec9a0bc00ff8e2c720dc883d1
mankinde23 <damilolamakinde493@gmail.com>
2024-06-22 10:00:55 +0100

    updated the favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,82 +7,13 @@ import { Montserrat } from "next/font/google";
 
 const inter = Poppins({ subsets: ["latin"], weight: "400" });
 
-// export const metadata = {
-//   title: "Sterling Homes",
-//   description:
-//     "Discover your dream home with Sterling Homes - your premier destination for real estate excellence. Explore a curated selection of properties and cooperative living spaces designed to meet your unique lifestyle needs. Experience seamless navigation, expert guidance, and unparalleled convenience in finding your perfect place to call home.",
-//   image: "/sterling-logo.png",
-// };
-
-// export const metadata = {
-//   title: "Sterling Homes",
-//   description:
-//     "Discover your dream home with Sterling Homes - your premier destination for real estate excellence. Explore a curated selection of properties and cooperative living spaces designed to meet your unique lifestyle needs. Experience seamless navigation, expert guidance, and unparalleled convenience in finding your perfect place to call home.",
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-32x32.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-//   openGraph: {
-//     title: "Sterling Homes",
-//     description: "Sterling Homesllc",
-//     images: [
-//       {
-//         url: "/sterling-logo.png",
-//         width: 800,
-//         height: 600,
-//         alt: "Sterling Homes Logo",
-//       },
-//     ],
-//   },
-// };
-// export const metadata = {
-//   metadataBase: "https://www.sterlinghomesllc.com/", // Replace 'yourdomain.com' with your actual domain
-//   title: "Sterling Homes",
-//   description:
-//     "Discover your dream home with Sterling Homes - your premier destination for real estate excellence.",
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-32x32.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-//   openGraph: {
-//     title: "Sterling Homes",
-//     description:
-//       "Discover your dream home with Sterling Homes - your premier destination for real estate excellence.",
-//     images: [
-//       {
-//         url: "/sterling-logo.png",
-//         width: 800,
-//         height: 600,
-//         alt: "Sterling Homes Logo",
-//       },
-//     ],
-//   },
-// };
 export const metadata = {
-  metadataBase: new URL("https://www.sterlinghomesllc.com"), // Replace with your actual domain
+  metadataBase: new URL("https://www.sterlinghomesllc.com"),
   title: "Sterling Homes",
   description:
     "Discover your dream home with Sterling Homes - your premier destination for real estate excellence.",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-32x32.png",
-    apple: "/apple-touch-icon.png",
-  },
-  openGraph: {
-    title: "Sterling Homes",
-    description:
-      "Discover your dream home with Sterling Homes - your premier destination for real estate excellence.",
-    images: [
-      {
-        url: "/sterling-logo.png",
-        width: 800,
-        height: 600,
-        alt: "Sterling Homes Logo",
-      },
-    ],
-  },
+  favicon: "/app/favicon.ico",
+  sizes: "32x32",
 };
 
 export const viewport = {
